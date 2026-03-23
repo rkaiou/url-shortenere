1c3c72c205c9550b183521214018583e0f2ea180
norton <102422616+norrtonn@users.noreply.github.com>
2026-03-23 17:51:12 +0400

    added model and create tables

--- app.py
+++ app.py
@@ -25,3 +25,21 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# Мodel
+
+class Link(Base):
+    __tablename__ = "links"
+
+    id = Column(Integer, primary_key=True, index=True)
+    original_url = Column(String, nullable=False)
+    short_code = Column(String(10), unique=True, index=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    expires_at = Column(DateTime, nullable=True)
+    click_count = Column(Integer, default=0)
+    last_accessed = Column(DateTime, nullable=True)
+    is_active = Column(Boolean, default=True)
+
+
+# Создание таблиц
+Base.metadata.create_all(bind=engine)
